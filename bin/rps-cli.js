63910fa5e40df15f1f3fe587c3e67242919f3861
thomaslb <lagaba@live.unc.edu>
2023-04-28 20:24:20 -0400

    Edited.

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -5,3 +5,37 @@ import minimist from "minimist";
 
 
 var args = minimist(process.argv.slice(2));
+
+if (args.h || args.help) {
+    console.log(`Usage: node-rps [SHOT]
+    Play Rock Paper Scissors (RPS)
+    
+      -h, --help      display this help message and exit
+      -r, --rules     display the rules and exit
+    
+    Examples:
+      node-rps        Return JSON with single player RPS result.
+                      e.g. {"player":"rock"}
+      node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                      e.g {"player":"rock","opponent":"scissors","result":"win"}`);
+    process.exit(0);
+}
+
+if(args.rules || args.r) {
+    console.log(`Rules for Rock Paper Scissors:
+  - Scissors CUTS Paper
+  - Paper COVERS Rock
+  - Rock CRUSHES Scissors`);
+    process.exit(0);
+}
+try {
+    console.log(JSON.stringify(rps(args._[0])));
+} catch (error) { 
+    console.log(`Rules for Rock Paper Scissors:
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock CRUSHES Scissors`);
+    process.exit(0);
+}
+
+process.exit(0);
